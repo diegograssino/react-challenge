3e7b018029589a9747009a60cf06022f6d499338
Diego Grassino <diegograssino@gmail.com>
2025-04-01 20:53:31 -0300

    Add sql query simulation crossing data on products endpoint

--- back/services/index.js
+++ back/services/index.js
@@ -2,7 +2,17 @@ import productsMock from "../mocks/products.js";
 import stockPriceMock from "../mocks/stock-price.js";
 
 export const productsService = () => {
-  return productsMock;
+  const result = productsMock.map((product) => {
+    const skus = product.skus.map((sku) => {
+      const stockPrice = stockPriceMock[sku.code] || { stock: 0, price: 0 };
+
+      return { code: sku.code, name: sku.name, ...stockPrice };
+    });
+
+    return { ...product, skus };
+  });
+
+  return result;
 };
 
 export const stockPriceBySKU = (sku) => {
